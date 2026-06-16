f95cfdf3cf90a7c5ecb570116fa182d6dc0764a6
ky0ta168 <kyouta1218@gmail.com>
2026-06-17 00:36:46 +0900

    deploy

--- docs/assets/react-D2kvPX3Z.js
+++ docs/assets/react-D2kvPX3Z.js
@@ -1,4 +1,4 @@
-import{n as e,r as t}from"./index-BZt5Wv95.js";import{n}from"./hls-rWZbFoUf.js";import{n as r,t as i}from"./dist-B59uHPHp.js";var a=t(e(),1),o=(e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{let{src:r,...i}=t;return`
+import{n as e,r as t}from"./index-BswpgDta.js";import{n}from"./hls-rWZbFoUf.js";import{n as r,t as i}from"./dist-B59uHPHp.js";var a=t(e(),1),o=(e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{let{src:r,...i}=t;return`
         <script type="application/json" id="config">
           ${JSON.stringify(n.config||{})}
         <\/script>
